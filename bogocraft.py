66576be12d5ee1407b97e025fd67fb0b8e32ae3c
Kevin Daudt <compufreak@gmail.com>
2011-09-26 09:20:28 +0200

    Use a commandline switch to enable profiling

--- bogocraft.py
+++ bogocraft.py
@@ -2,6 +2,14 @@ __author__ = 'ikke'
 
 import pygame
 import cProfile
+import argparse
+import sys
+
+parser = argparse.ArgumentParser(description="Start bogocraft")
+
+parser.add_argument("--profile", dest="profile", default=False, help="Enables profile output", nargs="?")
+parser.add_argument('-p', dest='profile', default='profile.out')
+args = parser.parse_args()
 
 config = {
     'caption': "Bogocraft - V0.0.1"
@@ -16,5 +24,8 @@ pygame.display.set_caption(config['caption'])
 from bc.game import Game
 
 g = Game(display, config)
-#g.run()
-cProfile.run('g.run()', 'profile.out')
+
+if args.profile == False:
+    cProfile.run('g.run()', args.profile or "profile.out")
+else:
+    g.run()
